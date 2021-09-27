dd1e861561486bbf644f381e5ddefcc5bf72f5df
Liuizn <Luizgabrielsnn@gmail.com>
2021-09-27 17:04:31 -0300

    aula 299-300

---
+++ Aulas/8_PHP/aula299.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula 299</title>
+</head>
+<body>
+    
+    <?php 
+        echo ("teste (tag padrão)");
+        <br><br>
+    ?>
+
+        <?= "teste (tag de impressão)"?>
+        
+              
+</body>
+</html>
