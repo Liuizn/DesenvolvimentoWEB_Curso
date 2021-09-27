d6a9f6b3272affbe264db1d58df1a8b2e6801c45
Liuizn <Luizgabrielsnn@gmail.com>
2021-09-27 17:05:46 -0300

    atividade 301-303

---
+++ Aulas/8_PHP/aula303.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 302</title>
+</head>
+<body>
+
+    <?php 
+
+    //Comentário no modo em Barra
+    # Comentário no modo sharp
+
+    /*
+    Comentando
+    várias
+    linhas
+    */
+
+    ?>
+</body>
+</html>
