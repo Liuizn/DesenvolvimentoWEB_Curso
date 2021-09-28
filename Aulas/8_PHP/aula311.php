cf51b9218e7befa3170680337f25cf0c7b26456a
Liuizn <Luizgabrielsnn@gmail.com>
2021-09-28 19:24:26 -0300

    aula sobre IF-ELSE

---
+++ Aulas/8_PHP/aula311.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+
+    # ==
+    # != || <>
+    #====
+    #!==
+
+        if(false){
+            echo ('se for true, cai nesse');
+        } else {
+            echo ('se for false, cai nesse');
+        }
+
+        echo (' <br> ');
+
+        if(2 == 2){
+            echo ('idênticos');
+        } else {
+            echo ('não idênticos');
+        }
+
+        echo (' <br> ');
+
+        if (2 != 3) {
+            echo ('são diferentes');
+        } else {
+            echo ("não são diferentes");
+        }
+
+        echo (' <br> ');
+
+        if (2 === 2){
+            echo ('são de tipos idênticos');
+        } else {
+            echo ('não são de tipo idênticos');
+        }
+
+        echo (' <br> ');
+
+        if (2 !== 2){
+            echo ('não são de tipos idênticos');
+        } else {
+            echo ('são de tipos idênticos');
+        }
+        
+        echo (' <br> ');
+
+    
+    ?>
+</body>
+</html>
