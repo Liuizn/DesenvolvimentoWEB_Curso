18c5af2980d13e49e46d0bc73479fc23e8991cc1
Luiz Gabriel Dos Santos Nogueira <luizgabrielsnn@gmail.com>
2021-11-10 17:50:49 -0300

    membros estáticos

---
+++ Aulas/9_OO_PHP/metodos_estaticos.php
@@ -0,0 +1,19 @@
+<?php 
+
+    class Exemplo 
+    {
+        public static $atributo1 = 'sou estático';
+        public $atributo2 = 'não sou estático';
+
+        public static function metodo1(){
+            echo "sou estático metodo";
+        }
+
+        public function metodo2(){
+            echo "não sou estático método";
+        }
+    }
+    
+    Echo Exemplo::$atributo1;
+
+?>
