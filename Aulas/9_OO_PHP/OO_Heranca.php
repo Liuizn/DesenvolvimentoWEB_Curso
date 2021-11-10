38db1c8b37fffd0551503d444b7012e2460a8e2d
Luiz Gabriel Dos Santos Nogueira <luizgabrielsnn@gmail.com>
2021-11-09 21:22:03 -0300

    herança

---
+++ Aulas/9_OO_PHP/OO_Heranca.php
@@ -0,0 +1,43 @@
+<?php 
+
+    class Veiculo 
+    {
+        public $placa = null;
+        public $cor = null;
+
+        function acelerar()
+        {
+        
+        }
+
+    }
+
+    class ClassName extends Veiculo
+    {
+        public $tetoSolar = null;
+        
+        function abrirTetoSolar()
+        {
+
+        }
+
+        function trocarPosicaoVolante()
+        {
+
+        }
+    }
+
+    class Moto extends Veiculo
+    {
+        public $contraPesoGuiado = null;
+
+        function empinar()
+        {
+            
+        }
+    }
+    
+    
+
+
+?>
