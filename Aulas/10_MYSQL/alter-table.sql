7538f38370b655b0f620718666c8019dd0569f14
Luiz Gabriel Dos Santos Nogueira <luizgabrielsnn@gmail.com>
2021-11-22 16:54:22 -0300

    insert new file

--- Aulas/10_MYSQL/alter-table.sql
+++ Aulas/10_MYSQL/alter-table.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE aula02;
-USE aula02;
+CREATE DATABASE aula03;
+USE aula03;
 CREATE TABLE tb_cursos (
 	id_curso INT NOT NULL,
     img_curso VARCHAR(100) NOT NULL,
@@ -9,4 +9,8 @@ CREATE TABLE tb_cursos (
     ativo BOOLEAN DEFAULT TRUE,
     preco FLOAT(8,2) DEFAULT 0
 );
+
+ALTER TABLE tb_cursos ADD COLUMN carga_horaria INT NOT NULL; #Adiciona
+ALTER TABLE tb_cursos CHANGE carga_horaria carga_horaria VARCHAR(5) NOT NULL; #Edita
+ALTER TABLE tb_cursos DROP carga_horaria; #Exclui
 
